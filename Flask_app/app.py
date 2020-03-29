400043aa0089ff875a737869daafa976fed50bdc
omkarsingh1008 <omkarsinghrajpurohit363@gmail.com>
2020-03-29 17:45:54 +0530

    second commit

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -15,33 +15,33 @@ def index():
 	death=pd.DataFrame(columns=['country','deaths'])
 	for i in range(5):
 		death.loc[i,'country']=l[i]['country']
-		death.loc[i,'deaths']=l[i]['latest']['deaths']
+		death.loc[i,'deaths']=l[i]['latest']['deaths']#top five deaths cases in world
 	l1 = covid19.getLocations(rank_by='confirmed')
 	con=pd.DataFrame(columns=['country','confirmed'])
 	for i in range(5):
 		con.loc[i,'country']=l1[i]['country']
-		con.loc[i,'confirmed']=l1[i]['latest']['confirmed']
+		con.loc[i,'confirmed']=l1[i]['latest']['confirmed']#top five confirmend cases in world
 
 
 	return render_template('index.html',death=death.to_html(),con=con.to_html())
 @app.route('/data/',methods=['POST'])
 def data():
 
-	num=request.form['fname']
+	num=request.form['fname'] # get city name
 
 	fe=['name','rating','user_ratings_total','vicinity']
-	city=num.replace(" ",'%20')
+	city=num.replace(" ",'%20') #we are use google api to retrive data
 	url="https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyDNqwMjAeyo8YyILwwNuSvuadLGf10gd9w"
-	url=url.format(city)
+	url=url.format(city) # here we are format a city name in url
 	page=requests.get(url)
-	if page.status_code == 200 : 
+	if page.status_code == 200 : #check data is get or not if code status is 200 means we get a data
 		citys = page.json()
 	else : 
 		print("Check you internet connectivity or input")
-	lat=citys['candidates'][0]['geometry']['location']['lat']
+	lat=citys['candidates'][0]['geometry']['location']['lat'] #here we get lat
 	lng=citys['candidates'][0]['geometry']['location']['lng']
 	fetures=['gas_station','grocery_or_supermarket','hospital','pharmacy','restaurant']
-	data1=pd.DataFrame(columns=['category','name','rating','user_ratings_total','vicinity'])
+	data1=pd.DataFrame(columns=['category','name','rating','user_ratings_total','vicinity'])#creating a new dataframe
 	index=0
 	for fet in fetures:
 		url="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={},{}&rankby=distance&type={}&key=AIzaSyDNqwMjAeyo8YyILwwNuSvuadLGf10gd9w"
@@ -55,15 +55,15 @@ def data():
 			data1.loc[index,'category']=fet
 			for j in fe:
 				try:
-					data1.loc[index,j]=data['results'][i][j]
+					data1.loc[index,j]=data['results'][i][j]#insert a data into a Dataframe
 				except:
 					data1.loc[index,j]=0
 			index=index+1
 	covid_19=pd.read_csv('D:\\hackathon_Adhoc\\covid19-corona-virus-india-dataset\\complete.csv')
-	covid_19=covid_19[covid_19['Name of State / UT']==citys['candidates'][0]['formatted_address'].split(',')[1][1:]]
+	covid_19=covid_19[covid_19['Name of State / UT']==citys['candidates'][0]['formatted_address'].split(',')[1][1:]] #get a confirmend  cases by state
 	helps=pd.read_csv('D:\\hackathon_Adhoc\\coronvavirushelplinenumber (1).csv')
 	try:
-		number=helps[helps['Name of the State']==citys['candidates'][0]['formatted_address'].split(',')[1][1:]]['Helpline Nos.'].values[0]
+		number=helps[helps['Name of the State']==citys['candidates'][0]['formatted_address'].split(',')[1][1:]]['Helpline Nos.'].values[0]#get a help line number
 	except:
 		number="don't have "
 	state=citys['candidates'][0]['formatted_address'].split(',')[1][1:]
